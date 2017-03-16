c5ba9ba2d9224eecd27798f0f31bb81f0015f9a4
Rutetsu <j.katigb@gmail.com>
2017-03-16 14:35:33 +0100

    Little Schemer Ch3. Cons the Magnificent //Java Implementation

--- JavaExercises/src/com/company/Main.java
+++ JavaExercises/src/com/company/Main.java
@@ -1,8 +1,7 @@
 package com.company;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
 /*
  * Cons the Magnificent Chapter - The Little Schemer
  *
@@ -22,38 +21,111 @@ public class Main {
 
         removeRecursively<Integer> tester = new removeRecursively<Integer>();
         System.out.println(tester.rem(3, test));
-    }
-}
+
+        removeRecursively<Integer> tester2 = new removeRecursively<Integer>();
+        Integer[][] ints = new Integer[][]{{123, 456, 9182}, {}, {112, 568, 1120}, {2391}};
+        System.out.println(tester2.firsts(ints));
 
 
-class removeRecursively<T>{
-
-
-    private List<T> tList = new ArrayList<T>();
-
-    List<T> rem(T t, List<T> c) {
-
-        //if c is empty then return the null tList
-        //base case
-        if(c.isEmpty()) {
-            return tList;
-        }
-        //if the first index in this iteration == t (the value to remove)
-        //then return continue recursing (but not add to tList)
-        //this is the remove step
-        else if (c.get(0) == t) {
-            return rem(t, c.subList(1, c.size()));
-        }
-        //continue recursing through the list, adding to tList and passing in
-        //smaller and smaller sublists
-        else {
-            System.out.println("Recursing to find " + t);
-            tList.add(c.get(0));
-            return rem(t, c.subList(1, c.size()));
-
-        }
+        removeRecursively<Integer> tester3 = new removeRecursively<Integer>();
+        Integer[][] ints2 = new Integer[][]{{1, 5519, 9182}, {}, {3, 568, 2}, {2391}, {33}, {91}, {1245}, {1, 4, 6, 7, 9}};
+        System.out.println(tester3.genericFirsts(ints2, 2));  //[9182, 2, 6]
 
     }
 
 
+    static class removeRecursively<T> {
+
+
+        private List<T> tList = new ArrayList<T>();
+
+        List<T> rem(T t, List<T> c) {
+
+            //if c is empty then return the null tList
+            //base case
+            if (c.isEmpty()) {
+                return tList;
+            }
+            //if the first index in this iteration == t (the value to remove)
+            //then return continue recursing (but not add to tList)
+            //this is the remove step
+            else if (c.get(0) == t) {
+                return rem(t, c.subList(1, c.size()));
+            }
+            //continue recursing through the list, adding to tList and passing in
+            //smaller and smaller sublists
+            else {
+                tList.add(c.get(0));
+                return rem(t, c.subList(1, c.size()));
+
+            }
+
+        }
+
+        List<T> firsts(T[][] arrayOfArrays) {
+
+            //base case
+            if (arrayOfArrays.length == 0) {
+                //returning a null list in the list of lists
+                return tList;
+            }
+
+
+            //if we hit an empty list in a list we
+            //continue the recursion without adding
+            //to tList
+            else if (arrayOfArrays[0].length == 0 ) {
+
+                return firsts(Arrays.copyOfRange(arrayOfArrays, 1, arrayOfArrays.length));
+
+            }
+
+            //add the first value of the list
+            //continue the recursion
+            else {
+
+                tList.add(arrayOfArrays[0][0]);
+                return firsts(Arrays.copyOfRange(arrayOfArrays, 1, arrayOfArrays.length));
+
+            }
+
+
+        }
+
+        List<T> genericFirsts(T[][] arrayOfArrays, int indexInList){
+            //base case
+            if (arrayOfArrays.length == 0) {
+                //returning a null list in the list of lists
+                return tList;
+            }
+
+
+            //if we hit an empty list in a list
+            // or if the length of the current list
+            // is < the index specified, we
+            //continue the recursion without adding to tList
+            else if (arrayOfArrays[0].length == 0 || arrayOfArrays[0].length <= indexInList) {
+
+
+                //ex. genericFirsts(([1, 3, 4, 5],[1], [3],[4]),0) -> genericFirsts([1],[3],[4])...
+                // tList = {1}
+                return genericFirsts(Arrays.copyOfRange(arrayOfArrays, 1, arrayOfArrays.length), indexInList);
+
+            }
+
+            else {
+
+                tList.add(arrayOfArrays[0][indexInList]);
+                return genericFirsts(Arrays.copyOfRange(arrayOfArrays, 1, arrayOfArrays.length), indexInList);
+
+            }
+
+
+
+
+        }
+
+
+    }
 }
+
